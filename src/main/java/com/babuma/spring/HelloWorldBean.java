eb4cf30384424e7adf61150300bb66c095d43f0d
kangxiongwei <kangxiongwei@163.com>
2016-01-14 11:54:29 +0800

    spring Hello World!

---
+++ src/main/java/com/babuma/spring/HelloWorldBean.java
@@ -0,0 +1,14 @@
+package com.babuma.spring;
+
+/**
+ * Created by IntelliJ IDEA
+ * Author: kangxiongwei1
+ * Date: 2016/1/14 11:52
+ */
+public class HelloWorldBean {
+
+    public String sayHello() {
+        return "Hello World!";
+    }
+
+}
